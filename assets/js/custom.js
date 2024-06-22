34b3a7793edf63c1ff9c2ba20c6f1ca94d0c5aab
Victor de Paiva <victordepaiva.rio@gmail.com>
2024-06-22 12:45:24 -0300

    Update custom.js
    Adicionado cálculo do scroll percentage

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,12 +2,20 @@
 document.addEventListener('DOMContentLoaded', function () {
   let lastScrollTop = 0;
   const header = document.getElementById('site-header');
+  const scrollPercentage = document.getElementById('scroll-percentage');
 
   // Adicione a classe 'fixed' ao header inicialmente
   header.classList.add('fixed');
 
   window.addEventListener('scroll', function () {
+    
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    let docHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollPercent = (scrollTop / docHeight) * 100;
+    scrollPercentage.textContent = Math.round(scrollPercent) + '%';
+    
+    
     if (scrollTop > lastScrollTop) {
       // Rolando para baixo
       header.classList.remove('fixed');
