0956c2d48c3dfaa7b263695d748c6c9b389a1dba
Victor de Paiva <victordepaiva.rio@gmail.com>
2024-06-22 12:09:03 -0300

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,13 +3,18 @@ document.addEventListener('DOMContentLoaded', function () {
   let lastScrollTop = 0;
   const header = document.getElementById('site-header');
 
+  // Adicione a classe 'fixed' ao header inicialmente
+  header.classList.add('fixed');
+
   window.addEventListener('scroll', function () {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     if (scrollTop > lastScrollTop) {
       // Rolando para baixo
+      header.classList.remove('fixed');
       header.style.top = '-100px'; // Ajuste conforme a altura do seu header
     } else {
       // Rolando para cima
+      header.classList.add('fixed');
       header.style.top = '0';
     }
     lastScrollTop = scrollTop;
